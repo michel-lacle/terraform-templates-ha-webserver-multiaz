4dca8c0644fc1026406d356e5b49bf06b678853d
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-20 19:12:35 -0400

    attach instances to alb listeners

--- ha_webserver/aws_lb_target_group.tf
+++ ha_webserver/aws_lb_target_group.tf
@@ -5,7 +5,6 @@ resource "aws_lb_target_group" "multi-instance_tg" {
   vpc_id   = aws_vpc.template-vpc.id
 }
 
-
 resource "aws_lb_listener" "multi-instance-listener" {
   load_balancer_arn = aws_lb.multi-instance-lb.arn
   port              = "80"
@@ -13,6 +12,19 @@ resource "aws_lb_listener" "multi-instance-listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.multi-instance_tg.arn}"
+    target_group_arn = aws_lb_target_group.multi-instance_tg.arn
   }
+}
+
+resource "aws_lb_target_group_attachment" "instance-1-tg-attach" {
+  target_group_arn = aws_lb_target_group.multi-instance_tg.arn
+  target_id        = aws_instance.public-ec2-webserver.id
+  port             = 80
+}
+
+resource "aws_lb_target_group_attachment" "instance-1-tg-attach" {
+  target_group_arn = aws_lb_target_group.multi-instance_tg.arn
+  target_id        =
+  aws_instance.public-ec2-webserver-2.id
+  port             = 80
 }
