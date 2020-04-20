306e1194b1b2b8beede13ff7b316fcdc832dc027
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-20 19:04:01 -0400

    add target group

--- ha_webserver/aws_lb_target_group.tf
+++ ha_webserver/aws_lb_target_group.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_target_group" "multi-instance_tg" {
-  name     = "multi-instance_tg"
+  name     = "multi-instance-tg"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "${aws_vpc.template-vpc.id}"
+  vpc_id   = aws_vpc.template-vpc.id
 }
