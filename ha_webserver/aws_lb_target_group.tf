ca2bc6f24c92aff40d3b82379c6389e04812644a
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-20 19:06:22 -0400

    add lb listener forwarder

--- ha_webserver/aws_lb_target_group.tf
+++ ha_webserver/aws_lb_target_group.tf
@@ -4,3 +4,15 @@ resource "aws_lb_target_group" "multi-instance_tg" {
   protocol = "HTTP"
   vpc_id   = aws_vpc.template-vpc.id
 }
+
+
+resource "aws_lb_listener" "multi-instance-listener" {
+  load_balancer_arn = aws_lb.multi-instance-lb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.multi-instance_tg.arn}"
+  }
+}
