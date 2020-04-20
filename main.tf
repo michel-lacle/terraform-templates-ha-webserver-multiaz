efa97bf266cbd837237667b524442792b4044516
michel <michel@papiamentos-MacBook-Pro.local>
2020-04-20 12:06:50 -0400

    create two ec2 instances in same az

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
 module "ha-multi-instance" {
   source = "./ha_webserver"
 
-  ami_id = ""
-  availability_zones = ["us-east-1a", "us-east-1b"]
+  providers = {
+    aws = aws.north-america
+  }
+
+  ami_id = "ami-0a887e401f7654935"
+  availability_zones = [
+    "us-east-1a",
+    "us-east-1b"]
   project = "terraform-templates-ha-webserver-multiaz"
   user_data_1 = "webserver_install_1.sh"
   user_data_2 = "webserver_install_2.sh"
